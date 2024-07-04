6b97cf6440640b7e911df2bc94f9a4feb94f7bfa
Conference-Calls <76670817+Conference-Calls@users.noreply.github.com>
2024-07-03 22:12:08 -0400

    make graph initialization more efficient in memory

--- src/main/scala/wc/PageRank.scala
+++ src/main/scala/wc/PageRank.scala
@@ -23,11 +23,15 @@ object PageRankMain {
 //    try { hdfs.delete(new org.apache.hadoop.fs.Path(args(1)), true) } catch { case _: Throwable => {} }
 		// ================
     val fakeEdges = sc.parallelize(for (i <- 1 to k) yield (i * k, 0))
-    val graph = sc.parallelize(for (i <- 1 to k - 1) yield
-      for (j <- 1 to k - 1) 
-        yield ((i - 1)*k + j, (i - 1)*k + j + 1)).flatMap(line => line)
+    // val graph = sc.parallelize(for (i <- 1 to k - 1) yield
+    //   for (j <- 1 to k - 1) 
+    //     yield ((i - 1)*k + j, (i - 1)*k + j + 1)).flatMap(line => line)
+    //              .union(fakeEdges)
+
+    val graph = sc.parallelize(for (i <- 1 to k - 1) yield i)
+                 .flatMap(n => for (i <- 0 to k - 1) yield (n + i, n + i + 1))
                  .union(fakeEdges)
-                 
+
     val ranks = sc.parallelize(for (i <- 0 to k*k) yield (i, if (i==0) 0 else 1/(k*k)))
 
     val textFile = sc.textFile(args(0))
