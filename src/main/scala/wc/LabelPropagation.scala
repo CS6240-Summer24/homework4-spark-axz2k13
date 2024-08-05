bd4b4c33d8270a2b615fa1a798363b17cc230082
Conference-Calls <76670817+Conference-Calls@users.noreply.github.com>
2024-08-04 23:53:13 -0400

    everything but the loop

--- src/main/scala/wc/LabelPropagation.scala
+++ src/main/scala/wc/LabelPropagation.scala
@@ -17,8 +17,7 @@ object LabelPropagationMain {
     }
     val conf = new SparkConf().setAppName("Word Count")
     val sc = new SparkContext(conf)
-    val k = 100
-    val ITERATIONS = 10
+    val MAX = 10000
 		// Delete output directory, only to ease local development; will not work on AWS. ===========
 //    val hadoopConf = new org.apache.hadoop.conf.Configuration
 //    val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
@@ -26,32 +25,60 @@ object LabelPropagationMain {
 		// ================
     val textFile = sc.textFile(args(0))
     // counts is map of node -> rank of node when counting edges involving node, ex if node 9 has the most edges = (9, 1)
-    val counts = textFile.flatMap(line => line.split("  "))
-                 .map(word => (word.asInstanceOf[Int], 1))
+    val counts = textFile.flatMap(line => line.split("\t"))
+                 .map(word => (word.toInt, 1))
+                 .filter(line => line._1 <= MAX || MAX < 0)
                  .reduceByKey(_ + _)
                  .sortBy(line => line._2)
                  .zipWithIndex()
                  .map(line => (line._1._1, line._2))
     // expensive operation to "rename" each node according to its ranking by edges
-    var edges = textFile.map(line => (line.split("  ")(0).asInstanceOf[Int], line.split("  ")(1).asInstanceOf[Int]))
+    // also reverses each edge but we don't care because it's an undirected graph
+    var edges = textFile.map(line => (line.split("\t")(0).toInt, line.split("\t")(1).toInt))
+                 .filter(line => MAX < 0 || (line._1 <= MAX && line._2 <= MAX))
                  .join(counts)
                  .map(line => (line._2._1, line._2._2))
                  .join(counts)
                  .map(line => (line._2._2.asInstanceOf[Int], line._2._1.asInstanceOf[Int]))
+                 .flatMap(line => Seq(line, (line._2, line._1)))
+                 .persist() // we reuse this RDD every iteration
     // expensive operation to "rename" each node according to its ranking by edges
-    var nodes = textFile.flatMap(line => line.split("  "))
+    var nodes = textFile.flatMap(line => line.split("\t"))
                  .distinct()
-                 .map(word => (word.asInstanceOf[Int], word.asInstanceOf[Int]))
+                 .map(word => (word.toInt, word.toInt))
+                 .filter(line => line._1 <= MAX || MAX < 0)
                  .join(counts)
                  .map(line => (line._2._2.asInstanceOf[Int], line._2._2.asInstanceOf[Int]))
-    val stop = false
+    var stop = false
+    var iterations = 0
     while (!stop) {
+      stop = true
+      iterations += 1
       // each row is outgoing edge, (node, current label)
       val outgoingEdges = nodes.join(edges).map(line => (line._2._2, (line._1, line._2._1)))
-      // each row is (node, current label), label of node at outgoing edge
-      val assertingLabels = outgoingEdges.join(nodes).map(line => ((line._2._1._1, line._2._1._2), line._2._2))
+      
+      val assertingLabels = outgoingEdges.join(nodes).map(line => (line._2._1, line._2._2))
                                 .reduceByKey((a, b) => if (a < b) a else b)
+                                .map(line => {
+                                  if (line._2 < line._1._2) {
+                                    stop = false
+                                    (line._1._1, line._2)
+                                  } else {
+                                    line._1
+                                  }
+                                })
+      val foo = assertingLabels.collect() // we need to trigger it now for stop to update
+      nodes = assertingLabels
     }
-    //counts.saveAsTextFile(args(1))
+    // save output groups
+    val componentSize = nodes.map(line => (line._2, 1)).reduceByKey(_ + _)
+    val componentEdges = nodes.join(edges).map(line => (line._2._1, 1)).reduceByKey(_ + _)
+    // components is component label, (size, # of edges)
+    val components = componentSize.join(componentEdges).join(counts.map(line => (line._2.asInstanceOf[Int], line._1)))
+                                  .map(line => (line._2._2, (line._2._1._1, line._2._1._2/2)))
+                                  .sortBy(line => line._2._1)
+    components.saveAsTextFile(args(1))
+    System.out.println(components.count())
+    System.out.println(iterations)
   }
 }
